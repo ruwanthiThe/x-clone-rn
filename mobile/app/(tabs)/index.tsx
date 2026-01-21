b0c4c9e09461d10b53a81a144e38db271d68de8a
ruwanthiThe <ruthennakoon@gmail.com>
2026-01-22 05:10:27 +0530

    create post functionality added

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,17 +1,36 @@
+import PostComposer from "@/components/PostComposer";
+
 import SignOutButton from "@/components/SignOutButton";
+
 import { useUserSync } from "@/hooks/useUserSync";
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { RefreshControl, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
-  useUserSync();
-  return (
-    <SafeAreaView className="flex-1">
-      <Text>HomeScreen</Text>
-      <SignOutButton />
-    </SafeAreaView>
-  )
-}
+  
 
-export default HomeScreen
+  useUserSync();
+
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
+        <Text className="text-xl font-bold text-gray-900">Home</Text>
+        <SignOutButton />
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerStyle={{ paddingBottom: 80 }}
+        
+      >
+        <PostComposer />
+        
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+export default HomeScreen;
