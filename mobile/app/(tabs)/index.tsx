e07543d625ef8a1a3f4db8165712574855d7d839
ruwanthiThe <ruthennakoon@gmail.com>
2026-01-22 04:17:17 +0530

    sync clerk with db

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import SignOutButton from "@/components/SignOutButton";
+import { useUserSync } from "@/hooks/useUserSync";
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  useUserSync();
   return (
     <SafeAreaView className="flex-1">
       <Text>HomeScreen</Text>
